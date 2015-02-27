c7ad5493cdaccb838d152f4899acf51108bb2849
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-27 13:36:38 +0000

    Before all tests will open a browser go to tumblr website and login

--- spec/tumbrspec_spec.rb
+++ spec/tumbrspec_spec.rb
@@ -6,6 +6,10 @@ describe "Tumblr" do
   before :all do
     @browser = Watir::Browser.new :firefox
     @browser.goto "http://tumblr.com"
+    @browser.link(:href, "https://www.tumblr.com/login").click
+    @browser.text_field(id: "signup_email").set "larry.jonesmrfakeffakelarry-jones2015@hotmail.com"
+    @browser.text_field(id: "signup_password").set "BzZsjXjUTpIcq0w"
+    @browser.button(id: "signup_forms_submit").click
   end
  
   it "should do this" do
